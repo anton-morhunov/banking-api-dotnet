c1cc0e5bbde484f81836db285c7f874c2675c201
Anton Morhunov <92028283+anton-morhunov@users.noreply.github.com>
2026-08-01 17:27:10 +0900

    feat: add clients page to desktop version

--- bank-desktop/src/ViewModels/ClientsViewModel.cs
+++ bank-desktop/src/ViewModels/ClientsViewModel.cs
@@ -1,6 +1,26 @@
+using System.Collections.ObjectModel;
+using bank_desktop.DTOs.Clients;
+using bank_desktop.src.Services.Interfaces;
+
 namespace bank_desktop.src.ViewModels;
 
 public class ClientsViewModel : BaseViewModel
 {
-    
+    private readonly IClientService _clientService;
+    public ObservableCollection<ClientResponseDto> Clients { get; set; }
+
+    public ClientsViewModel(IClientService clientService)
+    {
+        _clientService = clientService;
+        
+        _ = LoadClientsAsync();
+    }
+
+    private async Task LoadClientsAsync()
+    {
+        var clients = await _clientService.GetAllClientsAsync();
+        
+        Clients = new ObservableCollection<ClientResponseDto>(clients);
+        OnPropertyChanged(nameof(Clients));
+    }
 }
