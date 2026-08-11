6426a3ab21359970e24ad47e79f4f2236b49411e
FujiKitKat <anton-morhunov@users.noreply.github.com>
2026-08-11 13:11:02 +0900

    feat: add correlationId to the logs

---
+++ src/BankAPI.API/Middleware/CorrelationMiddleware.cs
@@ -0,0 +1,38 @@
+namespace BankAPI.Middleware;
+public class CorrelationMiddleware
+{
+    private const string HeaderName = "X-Correlation-Id";
+    private readonly RequestDelegate _next;
+    private  readonly ILogger<CorrelationMiddleware> _logger;
+    
+    public CorrelationMiddleware(
+        RequestDelegate next,  
+        ILogger<CorrelationMiddleware> logger
+        )
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var correlationId = context.Request.Headers.TryGetValue(HeaderName, out var header)
+            ? header.ToString()
+            : Guid.NewGuid().ToString();
+        
+        context.Items[HeaderName] = correlationId;
+        
+        context.Response.Headers[HeaderName] = correlationId;
+
+        using (_logger.BeginScope(
+                   new Dictionary<string, object>
+                   {
+                       ["CorrelationId"] = correlationId
+                   }))
+        {
+            _logger.LogInformation("CorrelationId: {CorrelationId} assigned", correlationId);
+        
+            await _next(context);
+        }
+    }
+}
